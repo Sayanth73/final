a792ae892d85c4c722bb196692da24f2ffd80022
Sayanth Shanmugalingam <sayanthshanmugalingam@Mr.sevran>
2023-11-02 19:58:16 +0100

    112

--- real_libft/libft.h
+++ real_libft/libft.h
@@ -22,6 +22,7 @@ typedef struct s_list
 	void			*content;
 	struct s_list	*next;
 }				t_list;
+t_list			*ft_lstnew(void *content);
 void			ft_striteri(char *s, void (*f)(unsigned int, char*));
 char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void			ft_putendl_fd(char *s, int fd);
